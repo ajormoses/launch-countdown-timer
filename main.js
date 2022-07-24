67c3f0cd29d0e834a96af18bce4dd1dedcd74f82
ajormoses <ajormoses@gmail.com>
2022-07-24 12:36:10 +0100

    updated

--- main.js
+++ main.js
@@ -4,19 +4,42 @@ const mins = document.querySelector(".mins-txt");
 const secs = document.querySelector(".secs-txt");
 
 const updateClock = () => {
-  var date = new Date();
-  days.innerHTML = date.getDay() < 10 ? "0" + date.getDay() : date.getDay();
-  hours.innerHTML =
-    date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
-  mins.innerHTML =
-    date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
-  secs.innerHTML =
-    date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+  window.setInterval(() => {
+    const launchDate = new Date("Jan 1 2023 00:00:00").getTime();
+
+    const today = new Date().getTime();
+
+    const currentMilliseconds = launchDate - today;
+
+    const seconds = 1000;
+
+    const minutes = seconds * 60;
+
+    const hours = minutes * 60;
+
+    const day = hours * 24;
+
+    let countDay = Math.floor(currentMilliseconds / day);
+
+    countDay = countDay < 10 ? "0" + countDay : countDay;
+
+    let countHour = Math.floor(currentMilliseconds % day) / hours;
+
+    countHour = countHour < 10 ? "0" + countHour : countHour;
+
+    let countMinutes = Math.floor((currentMilliseconds % hours) / minutes);
+
+    countMinutes = countMinutes < 10 ? "0" + countMinutes : countMinutes;
+
+    let countSeconds = Math.floor((currentMilliseconds % minutes) / seconds);
+
+    countSeconds = countSeconds < 10 ? "0" + countSeconds : countSeconds;
+
+    days.innerHTML = countDay;
+    hours.innerHTML = countHour;
+    mins.innerHTML = countMinutes;
+    secs.innerHTML = countSeconds;
+  }, 1);
 };
 
-const initClock = () => {
-  updateClock();
-  window.setInterval("updateClock()", 1);
-};
-
-initClock();
+updateClock();
